7f9098f77b143cfe0f895f0ce9747c56c936fe5b
Érico Andrei <erico@simplesconsultoria.com.br>
2012-12-01 10:06:59 -0200

    Back to development: 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 import os
 
-version = '0.1'
+version = '0.2.dev0'
 
 long_description = (open("README.rst").read() + "\n" +
                     open(os.path.join("docs", "CONTRIBUTORS.txt")).read() + "\n" +
