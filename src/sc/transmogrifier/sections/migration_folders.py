ee8ffe68534c54a6f4318b5eea8ef04faf32a53e
João S. O. Bueno <gwidion@gmail.com>
2013-04-03 16:34:06 -0300

    Adds functionality to make use of the Wormhoke for authomatic folder creation

--- src/sc/transmogrifier/sections/migration_folders.py
+++ src/sc/transmogrifier/sections/migration_folders.py
@@ -5,11 +5,9 @@
 # Adaptation to time-warping on-demand folder migration
 # by jsbueno@simplesconsultoria.com.br
 
-
+import ast
 
 from zope.interface import classProvides, implements
-from collective.transmogrifier.interfaces import ISectionBlueprint
-from collective.transmogrifier.interfaces import ISection
 from collective.transmogrifier.utils import defaultMatcher
 
 from sc.transmogrifier import logger
@@ -28,6 +26,11 @@ class MigrationFoldersSection(BluePrintBoiler):
         self.folderType = self.options.get('folder-type', 'Folder')
         self.cache = self.options.get('cache', 'true').lower() == 'true'
 
+        self.use_wormhole = ast.literal_eval(
+                self.options.get("use_wormhole", "False"))
+        self.use_original_path = ast.literal_eval(
+                self.options.get("original_path", "False"))
+
         # blueprint initialization
         self.seen = set()
         self.traverse = self.transmogrifier.context.unrestrictedTraverse
@@ -44,6 +47,10 @@ class MigrationFoldersSection(BluePrintBoiler):
                 yield new_item
 
     def transmogrify(self, item):
+
+        if "__item_encapsulated_by_sc_migration_folders" in item:
+            return [item["__item_encapsulated_by_sc_migration_folders"]]
+
         traverse = self.traverse
 
         items = []
@@ -95,5 +102,28 @@ class MigrationFoldersSection(BluePrintBoiler):
         if self.cache:
             self.seen.add("%s/%s" % (container, id,))
 
+        if self.use_wormhole and items:
+            # Send our folders back to the begining of the pipeline -
+            # and put our item on a cocoon from where we will
+            # free it again in the future
+            # TODO: refactor the "cocoon" mechanism to
+            # be more integrated in the wormhole engine
+
+            # can't simply push our stuff to the end of the
+            # wormhole - if the wormhole is not empty, it may
+            # contain other items scheduled to be build after
+            # the current item. And the current item needs
+            # these folders to go first:
+            cocoon = {"__item_encapsulated_by_sc_migration_folders": item}
+            self.storage["wormhole"].appendleft(cocoon)
+            for new_folder in reversed(items):
+                self.storage["wormhole"].appendleft(new_folder)
+
+
+            #And...hyperjump back to the beggining of the pipeline, where
+            # our item will be hapily yielded by the whitehole blueprint
+            raise ThouShallNotPass
+
         items.append(item)
+
         return items
