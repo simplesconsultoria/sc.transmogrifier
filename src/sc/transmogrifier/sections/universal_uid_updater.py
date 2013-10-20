028f8462f5291120b44e20d8823dc3cdd9741bfb
Joao S. O. Bueno <jsbueno@simplsconsultoria.con.br>
2013-10-20 10:29:51 -0200

    Made universal UID setter even more Universal

--- src/sc/transmogrifier/sections/universal_uid_updater.py
+++ src/sc/transmogrifier/sections/universal_uid_updater.py
@@ -14,6 +14,14 @@ from sc.transmogrifier.utils import blueprint
 from sc.transmogrifier.utils import ThouShallNotPass, NothingToDoHere
 
 
+# Too cool, but there are objects in the Wild *
+# not setting thsoe interfaces! :-p
+
+# (*) for *.gov.br values of "Wild"
+
+DEFAULT_UID_ATTR = "_plone.uuid"
+
+
 @blueprint("sc.transmogrifier.universal_uid_updater")
 class UniversalUIDUpdater(BluePrintBoiler):
     """Sets UID for both AT and DX content types
@@ -38,24 +46,33 @@ class UniversalUIDUpdater(BluePrintBoiler):
 
         at_uid = ATIReferenceable.providedBy(obj)
         dx_uid = DXIReferenceable.providedBy(obj)
-        if at_uid or dx_uid:
-            old_uid = obj.UID()
-            if old_uid != uid:
-                # Code from plone.app.transmogrifier used for AT objects:
-                if at_uid:
-                    if not old_uid:
-                        setattr(obj, AT_UUID_ATTR, uid)
-                    else:
-                        obj._setUID(uid)
-                elif dx_uid:
-                    setattr(obj, DX_UID_ATTR, uid)
+
+        old_uid = obj.UID()
+        if old_uid != uid:
+            # Code from plone.app.transmogrifier used for AT objects:
+            if at_uid:
+                if not old_uid:
+                    setattr(obj, AT_UUID_ATTR, uid)
+                else:
+                    obj._setUID(uid)
+            elif dx_uid:
+                setattr(obj, DX_UID_ATTR, uid)
+            else: #Don't ask, JUST DO IT!
+                  # If the attribute is not used as UID, it
+                  # is not used as anything else as well,
+                  # and at least the desired UID value stays recorded in the
+                  # object, allowing for a post-migration retrieval
+                setattr(obj, DEFAULT_UID_ATTR, uid)
         return item
 
 
 """
 Fo checkign and setting the UID in dexterity items,
-the following quiestions in stackoverflow where consulted:
+the following topics in stackoverflow where consulted:
 
 http://stackoverflow.com/questions/17425668/plone-dexterity-behaviors-referenceablebehavior-not-referenceable
 http://stackoverflow.com/questions/14955747/set-uid-for-dexterity-type
+
+As for the "default value" - there is no place to check - the World can be rough sometimes
+
 """
